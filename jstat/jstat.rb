16b0c42d6f09bde1f81e9664b2f77dd0e7294a50
Derek Haynes <derek.haynes@gmail.com>
2012-11-29 14:15:45 -0700

    removed puts debugging line

--- jstat/jstat.rb
+++ jstat/jstat.rb
@@ -19,7 +19,6 @@ class Jstat < Scout::Plugin
     metrics = Hash[*headers.split.map(&:to_sym).zip(output.split).flatten]
     if option(:columns)
       columns = option(:columns).split(/[,\s]+/).map(&:to_sym)
-      puts "Columns: #{columns.join('|')}"
       metrics = Hash[metrics.select{|key, value| columns.include?(key)}]
     end
     report(metrics)
