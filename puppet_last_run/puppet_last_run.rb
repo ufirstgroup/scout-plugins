74ef88bf3c15f00e1aa9a8711294094e25220bb3
Derek Haynes <derek.haynes@gmail.com>
2013-02-12 14:26:57 -0700

    Reverting option name, adding minutes since last run

--- puppet_last_run/puppet_last_run.rb
+++ puppet_last_run/puppet_last_run.rb
@@ -2,7 +2,8 @@ class PuppetLastRun < Scout::Plugin
   needs 'yaml'
 
 OPTIONS=<<-EOS
-  data_file:
+  recent_runs_file:
+    label: Data File
     default: /var/lib/puppet/state/last_run_summary.yaml
     notes: "Either the path to a last_run_summary.yaml file or the recent_runs.yaml file (prior versions)."
 EOS
@@ -10,7 +11,7 @@ EOS
   DEFAULT_RECENT_RUNS_FILE="/var/lib/puppet/state/recent_runs.yaml"
 
   def build_report
-    recent_runs_file = option(:data_file) || DEFAULT_RECENT_RUNS_FILE
+    recent_runs_file = option(:recent_runs_file) || DEFAULT_RECENT_RUNS_FILE
 
     if File.exist?(recent_runs_file)
       actual_last_run_time = File.mtime(recent_runs_file)
@@ -36,12 +37,14 @@ EOS
       last_exit_code = data.last || 4
       report(:success => (last_exit_code <= 2) ? 1 : 0)
     else
+      minutes_since_last_run = (Time.now.to_i - data['time']['last_run'])/60 if (data['time'] and data['time']['last_run'])
       report(
         :events_total => data['events']['total'],
         :events_failure => data['events']['failure'],
         :resources_total => data['resources']['total'],
         :resources_failure => data['resources']['failed'],
-        :changes_total => data['changes']['total']
+        :changes_total => data['changes']['total'],
+        :minutes_since_last_run => minutes_since_last_run
       )
     end
   end
