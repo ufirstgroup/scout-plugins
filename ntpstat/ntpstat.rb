9f86c17306f5c3e8ed823e136864a63784d31262
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2013-05-29 11:01:16 -0600

    Use the original key for synchronized in ntpstat

--- ntpstat/ntpstat.rb
+++ ntpstat/ntpstat.rb
@@ -2,7 +2,7 @@ class NTPStat < Scout::Plugin
   def build_report
     output = `ntpstat`
     if $?.success?
-      report(:synchronized => 1)
+      report(:NSync => 1)
 
       within = output[/time correct to within (\d+) ms/, 1].to_i
       report(:accuracy_in_milliseconds => within)
@@ -10,7 +10,7 @@ class NTPStat < Scout::Plugin
       interval = output[/polling server every (\d+) s/, 1].to_i
       report(:polling_interval_in_seconds => interval)
     else
-      report(:synchronized => 0)
+      report(:NSync => 0)
     end
   rescue => boom
     error(boom.message)
