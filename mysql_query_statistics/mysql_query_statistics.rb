5505b399c3a1b0b4f5e6d669ab6ef4178ed650c7
Mr. Matt <system9@composition9.com>
2013-08-19 14:45:46 -0600

    mysql password escape

--- mysql_query_statistics/mysql_query_statistics.rb
+++ mysql_query_statistics/mysql_query_statistics.rb
@@ -85,7 +85,7 @@ class MysqlQueryStatistics < Scout::Plugin
   def connection_options
     opts = {'u' => get_option(:user) || 'root'}
     if p = get_option(:password)
-      opts['p'] = '"'+p.shellescape+'"' # quote the password, and escape quotes within the password
+      opts['p'] = '"' + p.gsub('"', '\"') + '"' # quote the password, and escape quotes within the password
     end
     opts['h'] = get_option(:host) if get_option(:host)
     opts['P'] = get_option(:port) if get_option(:port)
